b7cc391d668cc03449cf073b92885a0e3bac8ba5
Alexandre Dupriez <adupriez@listen-too.com>
2025-01-22 22:42:04 +0100

    feat(error): Improved validation error

--- src/controllers/zodiac.controller.ts
+++ src/controllers/zodiac.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { ZodiacService } from "@services/zodiac.service";
 import { ERRORS } from "@constants/errors";
+import { ValidationError } from "@errors/validation-error";
 
 const zodiacService = new ZodiacService();
 
@@ -9,13 +10,17 @@ export class ZodiacController {
     try {
       const birthdate = req.query.birthdate as string;
       if (!birthdate) {
-        return res.status(400).json({ error: ERRORS.MISSING_BIRTHDATE });
+        throw new ValidationError(ERRORS.MISSING_BIRTHDATE);
       }
 
       const zodiacSign = zodiacService.getZodiacSign(birthdate);
       res.json({ zodiacSign });
-    } catch (error: any) {
-      res.status(400).json({ error: error.message });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        res.status(400).json({ error: error.message });
+      } else if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      }
     }
   }
 }
