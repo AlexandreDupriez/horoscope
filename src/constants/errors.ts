184a8f91f5cdd08d087903316aa71827651e740b
Alexandre Dupriez <adupriez@listen-too.com>
2025-01-22 22:14:20 +0100

    feat(errors): added errors constants messages

---
+++ src/constants/errors.ts
@@ -0,0 +1,6 @@
+export const ERRORS = {
+  MISSING_BIRTHDATE:
+    "The birthdate query parameter is required and must follow the format YYYY-MM-DD",
+  INVALID_FORMAT: "birthdate format must be YYYY-MM-DD",
+  INVALID_DATE: "birthdate must be a valid date",
+};
