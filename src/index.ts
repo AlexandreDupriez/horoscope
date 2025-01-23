9cdf5f5ea36ae753a44757bde82519b1cbd3ce63
Alexandre Dupriez <adupriez@listen-too.com>
2025-01-23 10:35:02 +0100

    feat(security): Added securities to express app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import zodiacRoutes from "@routes/zodiac.routes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 10 * 1000,
+  max: 100,
+});
+
+app.use(helmet());
+app.use(limiter);
 app.use(zodiacRoutes);
 
 if (process.env.NODE_ENV !== "test") {
